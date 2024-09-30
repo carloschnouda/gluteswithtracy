87bdf4d839de6e885a41ccdd50252586eaf03b7a
carloschnouda <carlos.chnouda@gmail.com>
2024-09-30 15:27:40 +0300

    Add user dashboard active state

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -24,15 +24,15 @@
                                 class="block truncate text-sm text-gray-500 dark:text-gray-400">{{ Auth::user()->email }}</span>
                         </div>
                         <ul class="py-2" aria-labelledby="user-menu-button">
-                            <li>
+                            <li class="mb-2">
                                 <a href="{{ route('dashboard') }}"
-                                    class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
+                                    class="{{ Route::currentRouteName() == 'dashboard' ? 'bg-[#f00c93] text-white' : '' }} block px-4 py-2 text-sm text-gray-700 hover:bg-[#f00c93] hover:text-white rounded">Dashboard</a>
                             </li>
                             <li>
                                 <form action="/logout" method="POST">
                                     @csrf
                                     <button type="submit"
-                                        class="block w-full px-4 py-2 text-start text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white">
+                                        class="block w-full px-4 py-2 text-start text-sm text-gray-700 hover:bg-[#f00c93] hover:text-white rounded">
                                         Sign out
                                     </button>
                                 </form>
