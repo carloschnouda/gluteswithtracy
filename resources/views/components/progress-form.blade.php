10ef5c55fba0116e4053c965586c4e07c6c42338
carloschnouda <carlos.chnouda@gmail.com>
2024-09-30 17:24:36 +0300

    Progress form empty last name

--- resources/views/components/progress-form.blade.php
+++ resources/views/components/progress-form.blade.php
@@ -21,7 +21,8 @@
             <div class="group relative z-0 mb-5 w-full">
                 <input type="text" name="last_name" id="floating_last_name"
                     class="peer block w-full appearance-none border-0 border-b-2 border-gray-300 bg-gray-100 px-1 py-1.5 text-sm text-gray-900 focus:border-[#f00c93] focus:outline-none focus:ring-0"
-                    placeholder=" " value="{{ Auth::user()->last_name }}" readonly />
+                    placeholder=" " value="{{ Auth::user()->last_name ? Auth::user()->last_name : ' ' }}"
+                    readonly='{{ Auth::user()->last_name ? true : false }}' />
                 <label for="floating_last_name"
                     class="absolute top-0 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:start-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:font-medium peer-focus:text-[#f00c93] rtl:peer-focus:translate-x-1/4 dark:text-gray-400">Last
                     name</label>
