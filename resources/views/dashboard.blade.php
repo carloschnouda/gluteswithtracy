2a2b89c86a0c48694c48b674bd0444907147c49b
carloschnouda <carlos.chnouda@gmail.com>
2024-10-07 16:06:23 +0300

    add warm up section

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -11,19 +11,25 @@
                     workout plan</h2>
             </header>
 
-            <div id="nutrition_section">
-                <h1 class="mt-10 text-3xl font-bold text-[#f00c93]" animate>{{ $settings['nutrition_section_title'] }}
-                </h1>
-                <div class="grid grid-cols-1 items-center gap-10 lg:grid-cols-2">
-                    <div class="description mt-5" animate style="transition-delay: 1s">
-                        {!! $settings['nutrition_section_description'] !!}
-                    </div>
-                    <div class="image">
-                        <img animate='right' style="transition-delay: 1.5s"
-                            src="{{ Storage::url($settings['nutrition_section_image']) }}" alt="Nutrition Image">
+            <!-- Nutrition section -->
+            <section id="nutrition_section">
+                <div>
+                    <h1 class="mt-10 text-3xl font-bold text-[#f00c93]" animate>
+                        {{ $settings['nutrition_section_title'] }}
+                    </h1>
+                    <div class="grid grid-cols-1 items-center gap-10 lg:grid-cols-2">
+                        <div class="description mt-5" animate style="transition-delay: 1s">
+                            {!! $settings['nutrition_section_description'] !!}
+                        </div>
+                        <div class="image">
+                            <img animate='right' style="transition-delay: 1.5s"
+                                src="{{ Storage::url($settings['nutrition_section_image']) }}" alt="Nutrition Image">
+                        </div>
                     </div>
                 </div>
-            </div>
+            </section>
+
+            <!-- Workout section -->
             <section id="workout" class="mt-10">
                 <h1 class="mb-10 text-center text-4xl font-bold text-[#f00c93]" animate>
                     {{ $settings['workout_section_title'] }}
@@ -64,6 +70,7 @@
                 </div>
             </section>
 
+            <!-- Ab Routine -->
             <section id="ab_routine" class="mt-20">
                 <h1 class="text-center text-4xl font-bold text-[#f00c93]" animate>{{ $settings['abs_section_title'] }}
                 </h1>
@@ -91,11 +98,27 @@
                     @endforeach
                 </div>
             </section>
+
+            <!-- Stretch message -->
             <section id="stretch_message" class="mt-20">
-                <h1 class="text-center text-5xl md:text-6xl font-bold bg-[#f00c93] text-[#fff] p-5 rounded tracking-wider" animate>
+                <h1 class="rounded bg-[#f00c93] p-5 text-center text-5xl font-bold tracking-wider text-[#fff] md:text-6xl"
+                    animate>
                     {{ $settings['stretch_message'] }}
                 </h1>
             </section>
+
+            <!-- Warm up section -->
+            <section id="warm_up" class="mt-20">
+                <div class="relative mt-10">
+                    <video id="warmUpVideo" src="{{ Storage::url($settings['warm_up_section_video']) }}"
+                        class="aspect-video w-full" controls></video>
+                </div>
+                <div class="description mt-20">
+                    {!! $settings['warm_up_section_description'] !!}
+                </div>
+            </section>
+
+            <!-- Membership section -->
             <section class="membership-section mt-20" animate>
                 {!! $settings['membership_section'] !!}
             </section>
